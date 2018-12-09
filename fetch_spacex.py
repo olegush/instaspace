85aab3421da9af4c352ca744fff84c546b2f6d7f
olegush <43958767+olegush@users.noreply.github.com>
2018-12-09 15:05:11 +0300

    improved the code

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -4,23 +4,23 @@ import json
 
 
 def fetch_spacex_latest_images(url):
-    request = requests.get(url)
-    json_data = json.loads(request.text)
+    response = requests.get(url)
+    json_data = json.loads(response.text)
     return json_data['links']['flickr_images']
 
 
 def download_image(url, dir, filename):
-    request = requests.get(url)
+    response = requests.get(url)
     if not os.path.isdir(dir):
         os.mkdir(dir)
     with open(os.path.join(dir, filename), 'wb') as file:
-        file.write(request.content)
+        file.write(response.content)
 
 
 if (__name__ == '__main__'):
-    dir = 'images'
+    img_dir = 'images'
     spacex_api = 'https://api.spacexdata.com/v3/launches/latest'
     for num, url_img in enumerate(fetch_spacex_latest_images(spacex_api), 1):
         print(url_img)
-        filename = 'spacex_latest_' + str(num) + '.jpg'
-        download_image(url_img, dir, filename)
+        filename = 'spacex_latest_{}.jpg'.format(str(num))
+        download_image(url_img, img_dir, filename)
